681f8ea7bbad5a0e36fd1c510223653d5b4078d6
David Scott <dave@recoil.org>
2021-05-02 17:05:55 +0100

    luv: write some notes
    Signed-off-by: David Scott <dave@recoil.org>

--- src/hostnet/luv_lwt.mli
+++ src/hostnet/luv_lwt.mli
@@ -1,3 +1,19 @@
+(* Both Lwt and Luv have their own schedulers which we run on separate Threads.
+   To use one API from another we enqueue it and send a notification to process jobs in the queue.
+
+   A typical example would be:
+
+```
+let do_some_io () : unit Lwt.t =
+    let t, u = Lwt.task () in
+    Luv_lwt.run_in_luv (fun () ->
+        (* Now we're in the Luv event loop *)
+        Luv.Do.something _ @@ Luv_lwt.run_in_lwt @@ Lwt.wakeup_later u
+    );
+    t
+```
+*)
+
 module type Remote_work_queue = sig
     type 'a t
     (** A queue of jobs to be executed inside inside a remote event loop *)
@@ -12,10 +28,12 @@ end
 module Run_in_lwt: Remote_work_queue
 
 val run_in_luv: (unit -> unit) -> unit
-(** [run_in_luv f] runs [f ()] in the default Luv event loop. *)
+(** [run_in_luv f] runs [f ()] in the default Luv event loop.
+    This assumes that Luv is not thread-safe and therefore we must interact with it from one thread. *)
 
 val run_in_lwt: (unit -> unit) -> unit -> unit
-(** [run_in_lwt f] runs [f ()] in the default Lwt event loop. *)
+(** [run_in_lwt f] runs [f ()] in the default Lwt event loop.
+    This assumes that Lwt is not thread-safe and therefore we must interact with it from one thread. *)
 
 val run: 'a Lwt.t -> 'a
 (** [run t] evaluates [t] with the default Luv event loop. *)
