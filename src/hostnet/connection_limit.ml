7c00cb4b935f756af2e6878659cfd9718551919f
David Scott <dave@recoil.org>
2021-05-09 11:21:48 +0100

    luv: add a mutex around the connection table
    Signed-off-by: David Scott <dave@recoil.org>

--- src/hostnet/connection_limit.ml
+++ src/hostnet/connection_limit.ml
@@ -6,52 +6,80 @@ let src =
 
 module Log = (val Logs.src_log src : Logs.LOG)
 
+(* Ensure the table is safe to access from both the Luv event loop and the Lwt event loop. *)
+let m = Mutex.create ()
+
+let with_mutex m f =
+  Mutex.lock m;
+  try
+    let result = f () in
+    Mutex.unlock m;
+    result
+  with e ->
+    Mutex.unlock m;
+    raise e
+
 let max = ref None
 
-let set_max x =
-  begin match x with
-    | None -> Log.info (fun f -> f "Removed connection limit")
-    | Some limit -> Log.info (fun f -> f "Updated connection limit to %d" limit)
-  end;
-  max := x
-
-let next_connection_idx =
+let next =
   let idx = ref 0 in
   fun () ->
     let next = !idx in
     incr idx;
     next
 
-    let connection_table = Hashtbl.create 511
+let table = Hashtbl.create 511
 
-let get_num_connections () = Hashtbl.length connection_table
+let set_max x =
+  with_mutex m
+    (fun () ->
+      begin match x with
+        | None -> Log.info (fun f -> f "Removed connection limit")
+        | Some limit -> Log.info (fun f -> f "Updated connection limit to %d" limit)
+      end;
+      max := x
+    )
+
+let get_num_connections () = with_mutex m (fun () -> Hashtbl.length table)
 
 let connections () =
-  let xs = Hashtbl.fold (fun _ c acc -> c :: acc) connection_table [] in
-  Vfs.File.ro_of_string (String.concat "\n" xs)
+  with_mutex m
+    (fun () ->
+      let xs = Hashtbl.fold (fun _ c acc -> c :: acc) table [] in
+      Vfs.File.ro_of_string (String.concat "\n" xs)
+    )
 
 let register_no_limit description =
-  let idx = next_connection_idx () in
-  Hashtbl.replace connection_table idx description;
-  idx
+  with_mutex m
+    (fun () ->
+      let idx = next () in
+      Hashtbl.replace table idx description;
+      idx
+    )
 
 let register =
-  let last_error_log = ref 0. in
-  fun description -> match !max with
-  | Some m when Hashtbl.length connection_table >= m ->
-    let now = Unix.gettimeofday () in
-    if (now -. !last_error_log) > 30. then begin
-      (* Avoid hammering the logging system *)
-      Log.warn (fun f ->
-          f "Exceeded maximum number of forwarded connections (%d)" m);
-      last_error_log := now;
-    end;
-    Error (`Msg "too many open connections")
-  | _ ->
-    Ok (register_no_limit description)
+  with_mutex m
+    (fun () ->
+      let last_error_log = ref 0. in
+      fun description -> match !max with
+      | Some m when Hashtbl.length table >= m ->
+        let now = Unix.gettimeofday () in
+        if (now -. !last_error_log) > 30. then begin
+          (* Avoid hammering the logging system *)
+          Log.warn (fun f ->
+              f "Exceeded maximum number of forwarded connections (%d)" m);
+          last_error_log := now;
+        end;
+        Error (`Msg "too many open connections")
+      | _ ->
+        Ok (register_no_limit description)
+    )
 
 let deregister idx =
-  if not(Hashtbl.mem connection_table idx) then begin
-    Log.warn (fun f -> f "Deregistered connection %d more than once" idx)
-  end;
-  Hashtbl.remove connection_table idx
+  with_mutex m
+    (fun () ->
+      if not(Hashtbl.mem table idx) then begin
+        Log.warn (fun f -> f "Deregistered connection %d more than once" idx)
+      end;
+      Hashtbl.remove table idx
+    )
